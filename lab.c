bef361af96092020bcbf9e9e2a1219cd33e3c249
Capi <paola.olguin@usach.cl>
2018-04-21 17:37:30 -0300

    Terminadas las funciones que permiten eliminar clientes del registro

--- lab.c
+++ lab.c
@@ -82,7 +82,7 @@ void crearArchivo(struct Cliente* clientesBultos, int size){
     fclose(file);
   }
 }
-/*Funcion que busca al cliente dado su nombre, apellidos y nombre de su mascota. Retorna el indice del cliente si lo encuentra, y -1 si no. Para esta funcion se asume que pueden haber clientes que tengan el mismo nombre, pero no que sus mascotas se llamen igual.*/
+/*Funcion que busca al cliente dado su nombre, apellidos y nombre de su mascota. Retorna el indice del cliente si lo encuentra, y -1 si no. Para esta funcion se asume que pueden haber clientes que tengan el mismo nombre y apellidos, pero en ese caso sus mascotas no se pueden llamar igual.*/
 int getCliente(struct Cliente* clientes, int size, char* nombre, char* apellido, char* apellidoM, char* mascota, char* especie){
   int i, j;
   for (i = 0; i < size; i++){
@@ -114,36 +114,88 @@ struct Cliente* clientesAeliminar(int* size){
   int i;
   char line[MAXLINE];
   struct Cliente* clientesAborrar;
+  printf("%s\n", "Ingrese la cantidad de clientes que desea eliminar de los registros.");
+  fgets(line, sizeof(line), stdin);
+  *size = atoi(line);
   clientesAborrar = (struct Cliente*)calloc(*size, sizeof(struct Cliente));
   for (i = 0; i < *size; i++){
     printf("%s\n", "Ingrese el cliente de la forma que esta en el archivo.");
     fgets(line, sizeof(line),stdin);
     sscanf(line, "%s %s %s %s %s %hu %s %hu %s %s", clientesAborrar[i].name , clientesAborrar[i].sur1, clientesAborrar[i].sur2, clientesAborrar[i].pet, clientesAborrar[i].species, &clientesAborrar[i].age, clientesAborrar[i].phone, &clientesAborrar[i].atentions, clientesAborrar[i].vaccines, clientesAborrar[i].date);
   }
-  //pruebas
-  for (i = 0; i < *size; i++){
-    printf("%s %s %s %s %s %hu %s %hu %s %s", clientesAborrar[i].name , clientesAborrar[i].sur1, clientesAborrar[i].sur2, clientesAborrar[i].pet, clientesAborrar[i].species, clientesAborrar[i].age, clientesAborrar[i].phone, clientesAborrar[i].atentions, clientesAborrar[i].vaccines, clientesAborrar[i].date);
-  }
   return clientesAborrar;
 }
-
-struct Cliente* eliminarClientes(){
-
+/*Funcion que revisa si un cliente esta dentro de un arreglo, en cuyo caso retorna el valor 1. Si no, retorna 0. Para esta funcion vale lo mismo que para getCliente, si dos clientes tienen el mismo nombre y apellido, no pueden tener mascotas del mismo nombre*/
+int belongs(struct Cliente* clientesArevisar, struct Cliente cliente, int size){
+  int i;
+  for (i = 0; i < size; i++){
+    if ( strcmp(clientesArevisar[i].name, cliente.name) == 0){
+      if (strcmp(clientesArevisar[i].sur1, cliente.sur1) == 0){
+        if (strcmp(clientesArevisar[i].sur2, cliente.sur2) == 0){
+          if (strcmp(clientesArevisar[i].pet, cliente.pet) == 0){
+            return 1;
+          }
+        }
+      }
+      else{
+        i++;
+      }
+    }
+    else{
+      i++;
+    }
+  }
+  return 0;
+}
+/*Funcion que se encarga de crear un arreglo nuevo, sin los clientes eliminados, los cuales se tienen registrados en otro arreglo. La memoria del arreglo original y del auxiliar para borrar se limpia antes de entregar el nuevo arreglo.*/
+struct Cliente* eliminarClientes(struct Cliente* clientes, struct Cliente* clientesAborrar, int size, int size2, int* sizeN){
+  if (size2 > size){
+    printf("%s\n", "Ha habido un error, el arreglo de elementos a eliminar no puede ser mas grande que el arreglo original. El programa terminara.");
+    freeArreglo(clientes);
+    freeArreglo(clientesAborrar);
+    exit(0);
+  }
+  else{
+    int i;
+    int j = 0;
+    *sizeN = size -size2;
+    struct Cliente* clientesNuevo;
+    clientesNuevo = (struct Cliente*)calloc(*sizeN, sizeof(struct Cliente));
+    while( j != (*sizeN)){
+      for (i = 0; i < size; i++){
+        if (belongs(clientesAborrar, clientes[i], size2) == 1){
+          i++;
+        }
+        else{
+          clientesNuevo[j] = clientes[i];
+          j++;
+        }
+      }
+    }
+    freeArreglo(clientes);
+    freeArreglo(clientesAborrar);
+    for( i = 0; i < (size - size2); i++){
+      printf("%s %s %s %s %s %hu %s %hu %s %s", clientesAborrar[i].name , clientesAborrar[i].sur1, clientesAborrar[i].sur2, clientesAborrar[i].pet, clientesAborrar[i].species, clientesAborrar[i].age, clientesAborrar[i].phone, clientesAborrar[i].atentions, clientesAborrar[i].vaccines, clientesAborrar[i].date);
+    }
+    return clientesNuevo;
+  }
 }
 
-//TODO eliminar, agregarRegistro, modificarRegistro
+//TODO agregarRegistro, modificarRegistro
 
 void menu(){
   struct Cliente* clientes;
   struct Cliente* clientes2;
+  struct Cliente* clientesNuevo;
   int size;
   int eliminar;
-  int agregar;
+  int sizeN;
+  //  int agregar;
   clientes = crearArreglo(&size);
-  crearArchivo(clientes, size);
   clientes2 = clientesAeliminar(&eliminar);
-  freeArreglo(clientes2);
-  freeArreglo(clientes);
+  clientesNuevo = eliminarClientes(clientes, clientes2, size, eliminar, &sizeN);
+  crearArchivo(clientesNuevo, sizeN);
+  freeArreglo(clientesNuevo);
 }
 
 int main(int argc, char const *argv[]) {
